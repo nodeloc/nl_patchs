a4ea33ac534591628faa483b652faf6fa44489d1
xypp <2952795729@qq.com>
2024-10-06 16:05:36 +0800

    管理员限制

--- src/Api/Controller/GetLoungeData.php
+++ src/Api/Controller/GetLoungeData.php
@@ -24,18 +24,22 @@ class GetLoungeData implements RequestHandlerInterface
     {
         $actor = RequestUtil::getActor($request);
         $attributes = [];
-        $attributes['loungeCounter'] =
-            intval($this->settings->get('nodeloc-nl-patchs.lounge_allow'))
-            -
-            (Discussion::where("user_id", $actor->id)
-                ->where('created_at', '>=', $this->carbonZoneHelper->now()->setTime(0, 0)->utc())
-                ->whereExists(function ($query) {
-                    $query->selectRaw("1")
-                        ->whereColumn('discussions.id', 'discussion_tag.discussion_id')
-                        ->from('discussion_tag')
-                        ->where('discussion_tag.tag_id', intval($this->settings->get('nodeloc-nl-patchs.lounge_id')));
-                })
-                ->count());
+        if ($actor->can("ignoreLoungeLimit")) {
+            $attributes['loungeCounter'] = 999;
+        } else {
+            $attributes['loungeCounter'] =
+                intval($this->settings->get('nodeloc-nl-patchs.lounge_allow'))
+                -
+                (Discussion::where("user_id", $actor->id)
+                    ->where('created_at', '>=', $this->carbonZoneHelper->now()->setTime(0, 0)->utc())
+                    ->whereExists(function ($query) {
+                        $query->selectRaw("1")
+                            ->whereColumn('discussions.id', 'discussion_tag.discussion_id')
+                            ->from('discussion_tag')
+                            ->where('discussion_tag.tag_id', intval($this->settings->get('nodeloc-nl-patchs.lounge_id')));
+                    })
+                    ->count());
+        }
         $attributes['loungeId'] = $this->settings->get('nodeloc-nl-patchs.lounge_id');
         return new JsonResponse($attributes);
     }
