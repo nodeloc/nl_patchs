717dae73a011d5dade8fb175a51364533df2ad0f
xypp <2952795729@qq.com>
2024-10-18 00:02:04 +0800

    修复点赞存在的bug

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,10 +1,21 @@
 import app from 'flarum/forum/app';
 import { hookLikeButton } from './hookLikeButton';
 import { overrideTagComposer } from './overrideTagComposer';
+import { override } from 'flarum/common/extend';
 app.initializers.add('nodeloc/nl-patchs', () => {
   hookLikeButton();
   overrideTagComposer();
 
   // 修复自动续期的装扮通知
   app.store.models.decorationStorePurchase.prototype.purchase_count = function () { return this.attribute("item_count") };
+
+  // 修复
+  override(app.notificationComponents.postLiked.prototype, 'excerpt', function (o) {
+    if (this.attrs.notification.subject()) {
+      if ((this.attrs.notification.subject() as any).contentPlain()) {
+        return o();
+      }
+    }
+    return '';
+  });
 });
