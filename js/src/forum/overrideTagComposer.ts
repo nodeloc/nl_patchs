9ac2cf73b2db3960f08178259f8af49928f93664
xypp <2952795729@qq.com>
2024-11-10 18:44:48 +0800

    异步填充

--- js/src/forum/overrideTagComposer.ts
+++ js/src/forum/overrideTagComposer.ts
@@ -6,17 +6,19 @@ import TagDiscussionModal from 'flarum/tags/forum/components/TagDiscussionModal'
 import Tag from 'flarum/tags/common/models/Tag';
 import extractText from 'flarum/common/utils/extractText';
 export function overrideTagComposer() {
-    extend(DiscussionComposer.prototype, 'oninit', function () {
-        let selectableTags: Tag[] = getSelectableTags(null);
-        const shareTag = selectableTags.find((tag) => tag.id() == app.forum.attribute("nodeloc-nl-patchs.share_id"));
-        if (shareTag && this.composer.fields.tags && this.composer.fields.tags.find((t: Tag) => t.id() == shareTag?.id())) {
-            const value = this.$('textarea').val() as string;
-            if (!value.includes("### 内容来源")) {
-                if (/^\s*$/.test(value) || confirm(extractText(app.translator.trans('nodeloc-nl-patchs.forum.share_tag_template')))) {
-                    this.$('textarea').val(shareTag.attribute("template") + "\n\n" + value);
+    extend(DiscussionComposer.prototype, 'oncreate', function () {
+        setTimeout((t: any) => {
+            let selectableTags: Tag[] = getSelectableTags(null);
+            const shareTag = selectableTags.find((tag) => tag.id() == app.forum.attribute("nodeloc-nl-patchs.share_id"));
+            if (shareTag && t.composer.fields.tags && t.composer.fields.tags.find((t: Tag) => t.id() == shareTag?.id())) {
+                const value = t.$('textarea').val() as string;
+                if (!value.includes("### 内容来源")) {
+                    if (/^\s*$/.test(value) || confirm(extractText(app.translator.trans('nodeloc-nl-patchs.forum.share_tag_template')))) {
+                        t.$('textarea').val(shareTag.attribute("template") + "\n\n" + value);
+                    }
                 }
             }
-        }
+        }, 1, this);
     });
     DiscussionComposer.prototype.chooseTags = function (this: DiscussionComposer) {
         let selectableTags: Tag[] = getSelectableTags(null);
