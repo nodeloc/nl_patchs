e29d48a53d13fb8df0e2df2b937d527b546e7732
xypp <2952795729@qq.com>
2024-11-10 18:36:49 +0800

    初始判断

--- js/src/forum/overrideTagComposer.ts
+++ js/src/forum/overrideTagComposer.ts
@@ -6,6 +6,18 @@ import TagDiscussionModal from 'flarum/tags/forum/components/TagDiscussionModal'
 import Tag from 'flarum/tags/common/models/Tag';
 import extractText from 'flarum/common/utils/extractText';
 export function overrideTagComposer() {
+    extend(DiscussionComposer.prototype, 'oninit', function () {
+        let selectableTags: Tag[] = getSelectableTags(null);
+        const shareTag = selectableTags.find((tag) => tag.id() == app.forum.attribute("nodeloc-nl-patchs.share_id"));
+        if (shareTag && this.composer.fields.tags && this.composer.fields.tags.find((t: Tag) => t.id() == shareTag?.id())) {
+            const value = this.$('textarea').val() as string;
+            if (!value.includes("### 内容来源")) {
+                if (/^\s*$/.test(value) || confirm(extractText(app.translator.trans('nodeloc-nl-patchs.forum.share_tag_template')))) {
+                    this.$('textarea').val(shareTag.attribute("template") + "\n\n" + value);
+                }
+            }
+        }
+    });
     DiscussionComposer.prototype.chooseTags = function (this: DiscussionComposer) {
         let selectableTags: Tag[] = getSelectableTags(null);
         const shareTag = selectableTags.find((tag) => tag.id() == app.forum.attribute("nodeloc-nl-patchs.share_id"));
