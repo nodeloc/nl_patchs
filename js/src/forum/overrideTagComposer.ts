428c5fda26694744e00867addc4c0970695df543
xypp <2952795729@qq.com>
2024-11-14 03:14:57 +0800

    不再使用模板

--- js/src/forum/overrideTagComposer.ts
+++ js/src/forum/overrideTagComposer.ts
@@ -7,16 +7,19 @@ import Tag from 'flarum/tags/common/models/Tag';
 import extractText from 'flarum/common/utils/extractText';
 export function overrideTagComposer() {
     extend(DiscussionComposer.prototype, 'oncreate', function () {
+        // @ts-ignore
+        this.shownShareTip = false;
         setTimeout((t: any) => {
             let selectableTags: Tag[] = getSelectableTags(null);
             const shareTag = selectableTags.find((tag) => tag.id() == app.forum.attribute("nodeloc-nl-patchs.share_id"));
             if (shareTag && t.composer.fields.tags && t.composer.fields.tags.find((t: Tag) => t.id() == shareTag?.id())) {
-                const value = t.$('textarea').val() as string;
-                if (!value.includes("### 内容来源")) {
-                    if (/^\s*$/.test(value) || confirm(extractText(app.translator.trans('nodeloc-nl-patchs.forum.share_tag_template')))) {
-                        t.$('textarea').val(shareTag.attribute("template") + "\n\n" + value);
-                    }
-                }
+                // @ts-ignore
+                if (!this.shownShareTip) {
+                    // @ts-ignore
+                    this.shownShareTip = true;
+                    alert(shareTag.attribute("template"));
+
+                };
             }
         }, 1, this);
     });
@@ -29,12 +32,12 @@ export function overrideTagComposer() {
             onsubmit: (tags: Tag[]) => {
                 this.composer.fields.tags = tags;
                 if (shareTag && tags.find(t => t.id() == shareTag?.id())) {
-                    const value = this.$('textarea').val() as string;
-                    if (!value.includes("### 内容来源")) {
-                        if (/^\s*$/.test(value) || confirm(extractText(app.translator.trans('nodeloc-nl-patchs.forum.share_tag_template')))) {
-                            this.$('textarea').val(shareTag.attribute("template") + "\n\n" + value);
-                        }
-                    }
+                    // @ts-ignore
+                    if (!this.shownShareTip) {
+                        // @ts-ignore
+                        this.shownShareTip = true;
+                        alert(shareTag.attribute("template"));
+                    };
                 }
                 this.$('textarea').focus();
             },
